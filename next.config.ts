6d7ab07c5689f157d406f2b4619f9202fd6eb2d6
Farrel Adel Mohammad <farrel.adel@gmail.com>
2026-07-09 22:48:40 +0700

    chore(docker): configure Next.js standalone and docker-compose orchestration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['sweet-hamster-legally.ngrok-free.app'],
   serverExternalPackages: [],
+  output: "standalone",
 };
 
 export default nextConfig;
